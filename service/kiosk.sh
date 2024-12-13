a0d4f6abb4abe565a50f52b7c8a5463446cd6a2f
Oren Cohen <orenc17@gmail.com>
2024-12-13 15:34:47 +0200

    Use midori instead of firefox/chromium for rpi-zero

--- service/kiosk.sh
+++ service/kiosk.sh
@@ -6,4 +6,4 @@ xset -dpms
 . /home/pi/.nvm/nvm.sh
 nvm use 14
 serve -s /home/pi/Nowify/ &
-/usr/bin/firefox --kiosk --disable-pinch http://localhost:3000
+/usr/bin/midori -e Fullscreen http://localhost:3000
