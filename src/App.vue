fd51600951896914fd8fe80a535f6ad3b089fee1
Oren Cohen <orenc17@gmail.com>
2024-12-16 16:10:27 +0200

    Add podcasts support

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ export default {
         auth: 'https://accounts.spotify.com/authorize',
         token: 'https://accounts.spotify.com/api/token',
         base: 'https://api.spotify.com/v1',
-        nowPlaying: 'me/player/currently-playing'
+        nowPlaying: 'me/player/currently-playing?additional_types=episode'
       },
       player: {
         playing: false,
--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -236,16 +236,30 @@ export default {
       /**
        * Store the current active track.
        */
-      this.playerData = {
-        playing: this.playerResponse.is_playing,
-        trackArtists: this.playerResponse.item.artists.map(
-          artist => artist.name
-        ),
-        trackTitle: this.playerResponse.item.name,
-        trackId: this.playerResponse.item.id,
-        trackAlbum: {
-          title: this.playerResponse.item.album.name,
-          image: this.playerResponse.item.album.images[0].url
+
+      if (this.playerResponse.currently_playing_type == 'track') {
+        this.playerData = {
+          playing: this.playerResponse.is_playing,
+          trackArtists: this.playerResponse.item.artists.map(
+            artist => artist.name
+          ),
+          trackTitle: this.playerResponse.item.name,
+          trackId: this.playerResponse.item.id,
+          trackAlbum: {
+            title: this.playerResponse.item.album.name,
+            image: this.playerResponse.item.album.images[0].url
+          }
+        }
+      } else if (this.playerResponse.currently_playing_type == 'episode') {
+        this.playerData = {
+          playing: this.playerResponse.is_playing,
+          trackArtists: [this.playerResponse.item.show.name],
+          trackTitle: this.playerResponse.item.name,
+          trackId: this.playerResponse.item.id,
+          trackAlbum: {
+            title: this.playerResponse.item.show.name,
+            image: this.playerResponse.item.show.images[0].url
+          }
         }
       }
     },
